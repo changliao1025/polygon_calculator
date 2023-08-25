a4c5b1477c9a117d1c7095436a92f4a7f1fb3422
changliao1025 <changliao1025@outlook.com>
2023-08-25 14:37:38 -0700

    initial

---
+++ polycal/pycase.py
@@ -0,0 +1,14 @@
+class polygoncase(object):
+    """
+    """
+    sFilename_source=None
+    sFilename_target=None
+
+
+    def __init__(self, aParameter):
+        return
+    
+    def __call__(self, aParameter):
+        return
+    
+    def 
